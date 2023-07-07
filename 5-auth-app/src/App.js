4a41dd563eacace5ed890f81ab91ed5933d72d9b
hitenlulla <hitenlulla99@gmail.com>
2023-07-07 19:15:39 +0530

    useEffect hook

--- 5-auth-app/src/App.js
+++ 5-auth-app/src/App.js
@@ -1,19 +1,35 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
+import Login from "./components/Login/Login";
+import Home from "./components/Home/Home";
+import MainHeader from "./components/MainHeader/MainHeader";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  // Handling side effect
+  useEffect(
+    () => {
+      // This check will trigger an infinite loop because the component re-renders when the state changes, and we change the state when the component is rendering - Infinite loop
+      // This is called a side-effect
+      if (localStorage.getItem("isLoggedIn") === "1") {
+        setIsLoggedIn(true);
+      }
+    },
+    [
+      // No dependencies means it will be run only once when the component is rendered
+    ]
+  );
+
   const loginHandler = (email, password) => {
     // We should of course check email and password
     // But it's just a dummy/ demo anyways
+    localStorage.setItem("isLoggedIn", "1");
     setIsLoggedIn(true);
   };
 
   const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
     setIsLoggedIn(false);
   };
 
--- 5-auth-app/src/components/Login/Login.js
+++ 5-auth-app/src/components/Login/Login.js
@@ -1,34 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import Card from '../UI/Card/Card';
-import classes from './Login.module.css';
-import Button from '../UI/Button/Button';
+import Card from "../UI/Card/Card";
+import classes from "./Login.module.css";
+import Button from "../UI/Button/Button";
 
 const Login = (props) => {
-  const [enteredEmail, setEnteredEmail] = useState('');
+  const [enteredEmail, setEnteredEmail] = useState("");
   const [emailIsValid, setEmailIsValid] = useState();
-  const [enteredPassword, setEnteredPassword] = useState('');
+  const [enteredPassword, setEnteredPassword] = useState("");
   const [passwordIsValid, setPasswordIsValid] = useState();
   const [formIsValid, setFormIsValid] = useState(false);
 
+  // We need to set the form validity when enteredEmail or enteredPassword changes
+  // This can also be treated as a side-effect because of dependency on other states
+  // hence we can use useEffect with dependencies
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      console.log("Validating form inputs");
+      // We don't want to validate form inputs very frequently, because this is usually an http call and can lead to network usage, hence we use a timeout function
+      setFormIsValid(
+        enteredEmail.includes("@") && enteredPassword.trim().length > 6
+      );
+    }, 500);
+
+    // Cleanup function - Gets called before every execution of the side-effect function
+    return () => {
+      console.log("Cleaning up");
+      // Removing the timeout once a keystroke is recieved
+      clearTimeout(timer);
+    };
+  }, [enteredEmail, enteredPassword]);
+
   const emailChangeHandler = (event) => {
     setEnteredEmail(event.target.value);
-
-    setFormIsValid(
-      event.target.value.includes('@') && enteredPassword.trim().length > 6
-    );
   };
 
   const passwordChangeHandler = (event) => {
     setEnteredPassword(event.target.value);
-
-    setFormIsValid(
-      event.target.value.trim().length > 6 && enteredEmail.includes('@')
-    );
   };
 
   const validateEmailHandler = () => {
-    setEmailIsValid(enteredEmail.includes('@'));
+    setEmailIsValid(enteredEmail.includes("@"));
   };
 
   const validatePasswordHandler = () => {
@@ -45,7 +57,7 @@ const Login = (props) => {
       <form onSubmit={submitHandler}>
         <div
           className={`${classes.control} ${
-            emailIsValid === false ? classes.invalid : ''
+            emailIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="email">E-Mail</label>
@@ -59,7 +71,7 @@ const Login = (props) => {
         </div>
         <div
           className={`${classes.control} ${
-            passwordIsValid === false ? classes.invalid : ''
+            passwordIsValid === false ? classes.invalid : ""
           }`}
         >
           <label htmlFor="password">Password</label>
