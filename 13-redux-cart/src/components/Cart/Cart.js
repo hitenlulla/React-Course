ac3d60d467b8a58fe7741079ff7c8caf150f97db
hitenlulla <hitenlulla99@gmail.com>
2023-07-13 01:34:30 +0530

    Setting up redux-toolkit app level

--- 13-redux-cart/src/components/Cart/Cart.js
+++ 13-redux-cart/src/components/Cart/Cart.js
@@ -1,16 +1,34 @@
-import Card from '../UI/Card';
-import classes from './Cart.module.css';
-import CartItem from './CartItem';
+import Card from "../UI/Card";
+import classes from "./Cart.module.css";
+import CartItem from "./CartItem";
+import { useSelector } from "react-redux";
 
 const Cart = (props) => {
+  const cartItems = useSelector((state) => state.cart.items);
+  const cartData =
+    cartItems.length > 0 ? (
+      <ul>
+        {cartItems.map((cartItem) => (
+          <CartItem
+            key={cartItem.id}
+            item={{
+              title: cartItem.name,
+              quantity: cartItem.quantity,
+              total: cartItem.totalPrice,
+              price: cartItem.price,
+              id: cartItem.id,
+            }}
+          />
+        ))}
+      </ul>
+    ) : (
+      <p>Cart Empty, Add some items</p>
+    );
+
   return (
     <Card className={classes.cart}>
       <h2>Your Shopping Cart</h2>
-      <ul>
-        <CartItem
-          item={{ title: 'Test Item', quantity: 3, total: 18, price: 6 }}
-        />
-      </ul>
+      {cartData}
     </Card>
   );
 };
--- 13-redux-cart/src/components/Cart/CartButton.js
+++ 13-redux-cart/src/components/Cart/CartButton.js
@@ -1,10 +1,19 @@
-import classes from './CartButton.module.css';
+import classes from "./CartButton.module.css";
+import { uiActions } from "../../store/ui-slice";
+import { useDispatch } from "react-redux";
+import { useSelector } from "react-redux";
 
 const CartButton = (props) => {
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
+  const dispatcher = useDispatch();
+  const toggleCartHandler = () => {
+    dispatcher(uiActions.toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{cartQuantity}</span>
     </button>
   );
 };
