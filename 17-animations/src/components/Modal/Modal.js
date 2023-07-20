5e1638f6de499416c8c5af5351e672d636d0626e
hitenlulla <hitenlulla99@gmail.com>
2023-07-20 17:04:34 +0530

    Animaiton using react-transition-group transition

--- 17-animations/src/components/Modal/Modal.js
+++ 17-animations/src/components/Modal/Modal.js
@@ -1,17 +1,44 @@
 import React from "react";
+import Transition from "react-transition-group/Transition";
 
 import "./Modal.css";
 
 const modal = (props) => {
-  const cssClasses = ["Modal", props.show ? "ModalOpen" : "ModalClosed"];
+  const animationTiming = {
+    enter: 400,
+    exit: 1000,
+  };
 
   return (
-    <div className={cssClasses.join(" ")}>
-      <h1>A Modal</h1>
-      <button className="Button" onClick={props.closed}>
-        Dismiss
-      </button>
-    </div>
+    // Using Transition component to animate Modal
+    <Transition
+      in={props.show}
+      timeout={animationTiming}
+      mountOnEnter
+      unmountOnExit
+    >
+      {/* transitionState can be 'entering', 'entered', 'exiting', 'exited' */}
+      {(transitionState) => {
+        const cssClasses = [
+          "Modal",
+          transitionState === "entering"
+            ? "ModalOpen"
+            : transitionState === "exiting"
+            ? "ModalClosed"
+            : null,
+        ];
+        // Updating classes based on transitionState
+
+        return (
+          <div className={cssClasses.join(" ")}>
+            <h1>A Modal</h1>
+            <button className="Button" onClick={props.closed}>
+              Dismiss
+            </button>
+          </div>
+        );
+      }}
+    </Transition>
   );
 };
 
